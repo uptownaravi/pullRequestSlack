1eb9b96bad40c90708d570c8ac250d8cc47582f0
Harish Aravindan <harisharavindan92@gmail.com>
2020-08-01 13:25:06 +0530

    lambda code and requirements

---
+++ app.py
@@ -0,0 +1,44 @@
+import requests
+import json
+import os
+
+githubk = os.environ['gitkey']
+slackurl = os.environ['slackNotification']
+
+def lambda_handler(event, context):
+    
+    print(event)
+    
+    try:
+        hookdata = json.loads(event['body'])
+    except:
+        print("not json")
+    
+    if ( hookdata['action'] == "opened" ):
+        prNumber = str(hookdata['number'])
+        diffUrl = hookdata['pull_request']['html_url']
+        data = "New pull request" + prNumber + " opened. Check the diff at " + diffUrl
+        headers = {'Content-type': 'application/json'}
+        response = requests.post(slackurl, json={"text":data}, headers=headers)
+        print(response)
+    elif ( hookdata['action'] == "reopened"):
+        prNumber = str(hookdata['number'])
+        diffUrl = hookdata['pull_request']['html_url']
+        data = "Pull request " + prNumber + " reopened. Check the diff at " + diffUrl
+        headers = {'Content-type': 'application/json'}
+        response = requests.post(slackurl, json={"text":data}, headers=headers)
+        print(response)
+    elif ( hookdata['action'] == "closed"):
+        prNumber = str(hookdata['number'])
+        data = "Pull request " + prNumber + " closed."
+        headers = {'Content-type': 'application/json'}
+        response = requests.post(slackurl, json={"text":data}, headers=headers)
+        print(response)
+    elif ( hookdata['action'] == "merged"):
+        prNumber = str(hookdata['number'])
+        data = "Pull request " + prNumber + " merged."
+        headers = {'Content-type': 'application/json'}
+        response = requests.post(slackurl, json={"text":data}, headers=headers)
+        print(response)
+    else:
+        print(hookdata)
